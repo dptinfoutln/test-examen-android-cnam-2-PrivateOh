efc721d50ea4eb696552b4428d8c9fa838d08f17
WARIN ANTOINE <antoine.warin@outlook.com>
2018-03-29 09:15:41 +0200

    Premier commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# test-examen-android-CNAM-awarin621
---
+++ app/src/main/java/com/example/awarin621/androidcnam_2examentest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.awarin621.androidcnam_2examentest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
